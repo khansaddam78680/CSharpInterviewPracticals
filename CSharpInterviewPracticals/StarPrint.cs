1a6a04083c4f683803b3d15d823d5f712c92835c
Saddam Khan <Saddam.Khan@ASCKPS.IN>
2019-01-10 14:00:22 -0800

    updated comments

--- CSharpInterviewPracticals/StarPrint.cs
+++ CSharpInterviewPracticals/StarPrint.cs
@@ -15,15 +15,15 @@ namespace TestConsole
             {
                 for (j = 0; j <= i; j++)
                 {
-                    // There Are two ways to do it.
-                    // 1st way is good enough if you know how many lines you have to print.
+                    /* There Are two ways to do it.
+                    1st way is good enough if you know how many lines you have to print.*/
                     //if (i == 4 || i == 2 || i == 0)
                     //{
                     //    Console.Write("*");
                     //}
 
-                    // 2nd Always use this way if you have to print in odd and even format.
-                    // 0 to print odd and 1 to print even
+                    /* 2nd Always use this way if you have to print in odd and even format.
+                     0 to print odd and 1 to print even */
                     if (i % 2 == 0)
                     {
                         Console.Write("*");
