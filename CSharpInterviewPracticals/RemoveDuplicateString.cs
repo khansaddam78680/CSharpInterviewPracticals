73b548af4ca8907957253f7dc60cd645669477f7
Vishal <vishal@topsinfosolutions.com>
2019-02-14 12:29:54 +0530

    Added description to the parent level and also to the child level.

--- CSharpInterviewPracticals/RemoveDuplicateString.cs
+++ CSharpInterviewPracticals/RemoveDuplicateString.cs
@@ -2,8 +2,16 @@ using System;
 
 namespace TestConsole
 {
+    /// <summary>
+    /// Program to Remove Duplicate Strings.
+    /// </summary>
     public static class RemoveDuplicateString
     {
+        /// <summary>
+        /// This function removes the duplicates from the given string. 
+        /// </summary>
+        /// <param name="strToRemove"></param>
+        /// <returns>New string without duplication</returns>
         public static string RemoveString(string strToRemove)
         {
             // Keeps all encountered letters to this string.
